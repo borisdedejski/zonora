9f2bd74e8097b5d8588322e3be60e7d5dafab72b
Boris Dedejski <borisdedejski@gmail.com>
2025-09-03 10:54:47 +0200

    Initial commit

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -272,16 +272,8 @@ function InviteTeamMember() {
 export default function SettingsPage() {
   return (
     <section className="flex-1 p-4 lg:p-8">
-      <h1 className="text-lg lg:text-2xl font-medium mb-6">Team Settings</h1>
-      <Suspense fallback={<SubscriptionSkeleton />}>
-        <ManageSubscription />
-      </Suspense>
-      <Suspense fallback={<TeamMembersSkeleton />}>
-        <TeamMembers />
-      </Suspense>
-      <Suspense fallback={<InviteTeamMemberSkeleton />}>
-        <InviteTeamMember />
-      </Suspense>
+      <h1 className="text-lg lg:text-2xl font-medium mb-6">Dashboard</h1>
+
     </section>
   );
 }
